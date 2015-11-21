b9c20979dcf5348fbc5b0a16718a7fec33312d16
dyngar <dyngar@dyngar.local>
2015-11-20 21:11:45 -0500

    not creating orig correctly yet

--- debian.sh
+++ debian.sh
@@ -1,13 +1,14 @@
 #! /bin/sh
-#DEBFOLDERNAME="toxcore_0.0.0+20150630.r3444"
-DEBFOLDERNAME="toxcore_0.0.0+20150323.r3271"
+DEBFOLDERNAMEa="toxcore_0.0.0+20150630.r3444"
+DEBFOLDERNAMEb="toxcore_0.0.0+20150323.r3271"
 cd toxcore
 make clean
 git pull kytvi2p master
 git pull upstream master
 git push origin master
 rm "../$DEBFOLDERNAME.orig.tar.xz"
-tar cf - ./ | xz -zf - > "../$DEBFOLDERNAME.orig.tar.xz"
+tar cf - ./ | xz -zf - > "../$DEBFOLDERNAMEa.orig.tar.xz"
+tar cf - ./ | xz -zf - > "../$DEBFOLDERNAMEb.orig.tar.xz"
 cp -R ./ "../$DEBFOLDERNAME"
 cd "../$DEBFOLDERNAME"
 dpkg-source --commit
