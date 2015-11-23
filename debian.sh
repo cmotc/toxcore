239ee6c0933fb100453055df0e4b5bcaf6cc26f2
dyngar <dyngar@dyngar.local>
2015-11-23 00:26:36 -0500

    minor

--- debian.sh
+++ debian.sh
@@ -1,14 +1,12 @@
 #! /bin/sh
-DEBFOLDERNAMEa="toxcore_0.0.0+20150630.r3444"
-DEBFOLDERNAMEb="toxcore_0.0.0+20150323.r3271"
+DEBFOLDERNAME="toxcore_0.0.0+20150630.r3444"
 cd toxcore
 make clean
 git pull kytvi2p master
 git pull upstream master
 git push origin master
 rm "../$DEBFOLDERNAME.orig.tar.xz"
-tar cf - ./ | xz -zf - > "../$DEBFOLDERNAMEa.orig.tar.xz"
-tar cf - ./ | xz -zf - > "../$DEBFOLDERNAMEb.orig.tar.xz"
+tar cf - ./ | xz -zf - > "../$DEBFOLDERNAME.orig.tar.xz"
 cp -R ./ "../$DEBFOLDERNAME"
 cd "../$DEBFOLDERNAME"
 dpkg-source --commit
