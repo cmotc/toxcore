86713800dd264d8f62122c6f35dfd4e6cca649fb
cmotc <cmotc@openmailbox.org>
2016-01-24 03:22:58 -0500

    pulled in updates

--- debian.sh
+++ debian.sh
@@ -1,32 +1,16 @@
 #! /bin/sh
-DEBFOLDERNAME="toxcore_0.0.0+20150630.r3444"
+#DEBFOLDERNAME="toxcore_0.0.0+20150630.r3444"
+DEBFOLDERNAME="toxcore$(date +%Y%m%d)"
 cd toxcore
 make clean
 git pull kytvi2p master
 git pull upstream master
 git push origin master
-<<<<<<< HEAD
 rm "../$DEBFOLDERNAME.orig.tar.xz"
 tar cf - ./ | xz -zf - > "../$DEBFOLDERNAME.orig.tar.xz"
 cp -R ./ "../$DEBFOLDERNAME"
 cd "../$DEBFOLDERNAME"
-=======
-rm "../$DEBFOLDERNAMEa.orig.tar.xz"
-rm "../$DEBFOLDERNAMEb.orig.tar.xz"
-tar cf - ./ | xz -zf - > "../$DEBFOLDERNAMEa.orig.tar.xz"
-tar cf - ./ | xz -zf - > "../$DEBFOLDERNAMEb.orig.tar.xz"
-cp -R ./ "../$DEBFOLDERNAMEa"
-cd "../$DEBFOLDERNAMEa"
->>>>>>> 6c62d5f107c5de435d4bfadbd0764cfe57dc700c
 dpkg-source --commit
 debuild -us -uc >> ../log
 cd ..
-
-cp -R ./ "../$DEBFOLDERNAMEb"
-cd "../$DEBFOLDERNAMEb"
-dpkg-source --commit
-debuild -us -uc >> ../log
-cd ..
-
-rm "../$DEBFOLDERNAMEa" -rf
-rm "../$DEBFOLDERNAMEb" -rf
+rm "../$DEBFOLDERNAME" -rf
