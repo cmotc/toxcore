98b924f9cf5ed94e78cf863143a1cf54da9ced0a
idk <idk@idk.local>
2016-04-24 15:04:27 -0400

    Sun Apr 24 15:04:26 EDT 2016

--- debian.sh
+++ debian.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 #DEBFOLDERNAME="toxcore_0.0.0+20150630.r3444"
 DEBFOLDERNAME="toxcore$(date +%Y%m%d)"
-cd toxcore
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 make clean
 git pull kytvi2p master
 git pull upstream master
